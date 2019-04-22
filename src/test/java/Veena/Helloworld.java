ee02bea8e15d3fc2ec680b95e81b6dbf80059c90
Veena <marojuveena@gmail.com>
2019-04-22 19:11:19 -0400

    test

---
+++ src/test/java/Veena/Helloworld.java
@@ -0,0 +1,13 @@
+package Veena;
+
+import org.junit.Test;
+
+public class Helloworld {
+    //junit annotations this is test method to run
+    @Test
+  public void   Myhelloworld(){
+      System.out.println("Hello World");
+  }
+
+
+}
