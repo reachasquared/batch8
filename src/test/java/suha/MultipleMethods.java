b27966f2f6fed1d5b53ad5d54d58cee2fa3fe025
Sampath Namburi <sampath.namburi@oracle.com>
2019-04-23 11:53:13 -0400

    Working with Variables example

--- src/test/java/suha/MultipleMethods.java
+++ src/test/java/suha/MultipleMethods.java
@@ -3,32 +3,37 @@ package suha;
 import org.junit.Test;
 
 public class MultipleMethods {//class begins
-  //junit annotations @test
+
+    //junit annotations @test
     @Test
-  public void myHome(){
+    public void myHome() {
         reception();
         mainHall();
         storeroom();
     }
-    public void reception(){//reception method begins
-    System.out.println("Reception");
-    System.out.println("---------");
-    System.out.println("1.Welcome Desk");
-    System.out.println("2.Telephone");
-    System.out.println("3.Register");
-        }//reception method ends
-    public void mainHall(){//mainHall method begins
-     System.out.println("MainHall");
-     System.out.println("---------");
-     System.out.println("1.WhiteBoard");
-     System.out.println("2.Tables");
-     System.out.println("3.Chairs");
+
+    public void reception() {//reception method begins
+        System.out.println("Reception");
+        System.out.println("---------");
+        System.out.println("1.Welcome Desk");
+        System.out.println("2.Telephone");
+        System.out.println("3.Register");
+    }//reception method ends
+
+    public void mainHall() {//mainHall method begins
+        System.out.println("MainHall");
+        System.out.println("---------");
+        System.out.println("1.WhiteBoard");
+        System.out.println("2.Tables");
+        System.out.println("3.Chairs");
     }//mainHall method ends
-    public void storeroom(){//storeRoom method begins
-    System.out.println("StoreRoom");
-    System.out.println("----------");
-    System.out.println("1.SafeBox");
-    System.out.println("2.Paints");
-    System.out.println("3.PaintBrushes");
+
+    public void storeroom() {//storeRoom method begins
+        System.out.println("StoreRoom");
+        System.out.println("----------");
+        System.out.println("1.SafeBox");
+        System.out.println("2.Paints");
+        System.out.println("3.PaintBrushes");
     }//storeRoom method ends
+
 }//class ends
